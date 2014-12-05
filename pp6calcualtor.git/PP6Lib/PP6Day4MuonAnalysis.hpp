b26c06256a129bb2839ebe6b4ab7ac95505eb877
Aaron Knight <A.D.Knight@warwick.ac.uk>
2014-12-05 10:23:53 +0000

    Week 4 Work

---
+++ pp6calcualtor.git/PP6Lib/PP6Day4MuonAnalysis.hpp
@@ -0,0 +1,19 @@
+// File - PP6 Day 4 Muon Analysis
+
+// Author - Aaron Knight
+
+// Brief - Declaration of PP6 Day 4 Muon Analysis operation
+
+
+
+#ifndef PP6CALCULATOR_PP6DAY4MUONANALYSIS_HH
+
+#define PP6CALCULATOR_PP6DAY4MUONANALYSIS_HH
+
+
+
+int pp6day4_muonanalysis();
+
+
+
+#endif // PP6CALCULATOR_PP6DAY4MUONANALYSIS_HH
